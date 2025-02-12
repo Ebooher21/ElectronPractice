1a3cc7a907aa74a782f6d8861163cac579cbd848
Elijah Booher <elijahdrake2003@gmail.com>
2025-02-12 10:51:08 -0800

    implemented devtools and garbage collection

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,6 +1,10 @@
+//following a tutorial on youtube - Bro code & code evolution
 console.log(`Chimichanga`);
-const { app, BrowserWindow } = require('electron')
-const create = require("got/dist/source/create");
+//window.alert doesn't work because window isn't recognized - using node.js
+//document.getSelection('H1').textContent = 'I want a chimichanga!';
+const electron = require('electron')
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -13,15 +17,21 @@ const createWindow = () => {
 
     })
     win.loadFile('index.html')
+    win.webContents.openDevTools();
+    //garbage collection
+    win.on('closed',() =>{
+        let win = null;
+    })
 }
 
 app.on('ready', createWindow)
-app.on('window-all-closed', () => {
+
+//for mac
+/*app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-
 app.on('activate', () =>{
    if (win === null){
        createWindow()
    }
-})
+})*/
